2a64d812cb86df9931fde95b8b76804df0e969f3
Hugo Rodrigez <hugor37@gmail.com>
2020-07-31 17:08:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,7 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-array = (element)
 array[index_number] = element
   return array[index_number]
   p array[index_number]
