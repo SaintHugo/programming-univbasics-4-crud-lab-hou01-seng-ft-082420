ed12e9a30afe8a97825ae7c3724952b13a6b6dd4
Hugo Rodrigez <hugor37@gmail.com>
2020-07-31 16:53:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,9 +26,7 @@ def remove_element_from_start_of_array(array)
 array.shift()
 end
 
-def retrieve_element_from_index(array, index_number)
-  array = []
-  return array[index_number]
+def retrieve_element_from_index(array, index_number)array[index_number]
 
 end
 
